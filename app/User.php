6c341773c57701bc9d24f5c305c28eeb9da8d04f
Daniel Austin <danielaustin718@gmail.com>
2019-06-05 20:14:30 +0100

    Created session class and made mobile number unique

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'email_verification_code', 'mobile_verification_code', 'id'
+        'password', 'remember_token', 'email_verification_code', 'mobile_verification_code'
     ];
 
     /**
